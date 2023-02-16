951b19c7702b7eaa8b2dfe538acb1b4624ed30e4
maicolsanti <smaicol02@gmail.com>
2023-02-16 14:36:12 +0100

    first commit

---
+++ bin/esercizio2.dart
@@ -0,0 +1,5 @@
+import 'package:esercizio2/esercizio2.dart' as esercizio2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${esercizio2.calculate()}!');
+}
---
+++ test/esercizio2_test.dart
@@ -0,0 +1,8 @@
+import 'package:esercizio2/esercizio2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
